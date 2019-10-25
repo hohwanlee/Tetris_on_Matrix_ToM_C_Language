4049d7ed70cf0e0420ab674e6ec2fc86e498d9af
HO HWAN LEE <lhw432@naver.com>
2019-10-25 16:52:48 -0400

    Added comments in Tetris.c
    // comments in Tetris.c
    
    ex.
    
    // Screen size. Putty has default of 24x80 characters can be shown.

--- Tetris/Tetris.c
+++ Tetris/Tetris.c
@@ -1,27 +1,32 @@
 #include<stdio.h>
 
+// Screen size. Putty has default of 24x80 characters can be shown.
 #define SCREEN_ROWS 24
 #define SCREEN_COLUMNS 80
 
+// We do have border like | - |, we need space between game and screen.
 #define PADDING_ROWS 0
 #define PADDING_COLUMNS 1
 
+// Square made of GAME_ROWS x COLUMNS is where the tetris block can reside.
 #define GAME_ROWS 23
 #define GAME_COLUMNS 10
 
 
-
+// Define screen as 2D array, and game map as 2D array.
 int screen[SCREEN_ROWS][SCREEN_COLUMNS] = { {0} };
 
 int game[GAME_ROWS][GAME_COLUMNS] = { {0,1,0,0,0,0,0,0,0,0},{1,1,1,0,0,0,0,0,0,0},{0} };
 
 
+// increase score when time pass or a line or more has cleared.
 int score = 0;;
 
 int nl() { // simply newline
 	printf("\n");
 }
 
+// drawBorder like | / \ - 
 int drawBorder(int i, int j) {
 	int bPrinted = 0;
 
@@ -70,6 +75,7 @@ int drawBorder(int i, int j) {
 }
 
 
+// Display with using printf. Check empty space and print " "
 int display() {
 	int i, j;
 
